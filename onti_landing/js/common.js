dff2751d093168370de6de6ff54d6cf6cc536e44
Shumakov <nialshum@gmail.com>
2021-08-08 20:39:48 +0500

    fix onti_landing

--- onti_landing/js/common.js
+++ onti_landing/js/common.js
@@ -3,13 +3,28 @@
 (() => {
 const featuresSlider = document.querySelector('.features-slider');
 const swiper = new Swiper(featuresSlider, {
-  slidesPerView: 5,
-  spaceBetween: 10,
   loop: true,
+  slidesPerView: 'auto',
+  loopedSlides: 5,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  spaceBetween: 10,
   speed: 4000,
   autoplay: {
     delay: 1,
     disableOnInteraction: false,
   },
+  // breakpoints: {
+  //   992: {
+  //     slidesPerView: 'auto',
+  //     spaceBetween: 10
+  //   },
+  //   768: {
+  //     slidesPerView: 'auto',
+  //   },
+  //   480: {
+  //     slidesPerView: 'auto',
+  //   },
+  // }
 });
 })();
