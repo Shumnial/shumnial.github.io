05ffeef5d5ad7583c6a7266dd3c57cb368454efa
Shumakov <nialshum@gmail.com>
2021-08-08 17:04:45 +0500

    fix onti_landing

--- onti_landing/js/common.js
+++ onti_landing/js/common.js
@@ -1,16 +1,15 @@
-(() => {
-  document.addEventListener("DOMContentLoaded", function() {
-  const featuresSlider = document.querySelector('.features-slider');
+'use strict';
 
-  new Swiper(featuresSlider, {
-    slidesPerView: 5,
-    spaceBetween: 10,
-    loop: true,
-    speed: 4000,
-    autoplay: {
-      delay: 1,
-      disableOnInteraction: false,
-    },
-  });
+(() => {
+const featuresSlider = document.querySelector('.features-slider');
+const swiper = new Swiper(featuresSlider, {
+  slidesPerView: 5,
+  spaceBetween: 10,
+  loop: true,
+  speed: 4000,
+  autoplay: {
+    delay: 1,
+    disableOnInteraction: false,
+  },
 });
 })();
